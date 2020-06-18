c84812458e49a6b701a56133b7621a3f35043f85
Eric Sellors <github@esellors.com>
2020-06-17 22:06:15 -0500

    initial

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+
     </div>
   );
 }
 
 export default App;
+
+/*
+[
+  {
+    name: 'Fido',
+    img: 'https://placeimg.com/200/200/animals'
+  },
+  {
+    name: 'Sparkles',
+    img: 'https://placeimg.com/200/200/animals'
+  },
+  {
+    name: 'Pookers',
+    img: 'https://placeimg.com/200/200/animals'
+  }
+]
+*/
